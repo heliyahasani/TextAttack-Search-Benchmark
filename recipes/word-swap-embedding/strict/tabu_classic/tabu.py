c7728f12abe1eececdd7f11186324c7a932cd029
Heliya Hasani <heliya@heraculesai.org>
2024-05-18 14:24:46 +0200

    tabu classic grid added

--- recipes/word-swap-embedding/strict/tabu_classic/tabu.py
+++ recipes/word-swap-embedding/strict/tabu_classic/tabu.py
@@ -9,45 +9,53 @@ sys.path.append(transformation_dir)
 
 
 class EmbeddingBasedTabuSearch(SearchMethod):
-    def perform_search(self, initial_result, tabu_list_size, tabu_tenur):
-        """Perform a focused tabu search on synonyms of words in the sentence, with tabu list and aspiration criteria."""
-        beam = [initial_result.attacked_text]
-        best_result = initial_result
-        tabu_list = []
-        tabu_list_size = tabu_list_size  # Example size, adjust based on your needs
-        tabu_tenur = tabu_tenur
-
-        while not best_result.goal_status == GoalFunctionResultStatus.SUCCEEDED:
-            potential_next_beam = []
-            for text in beam:
-                transformations = self.get_transformations(
-                    text, original_text=initial_result.attacked_text
+    def perform_search(self, initial_result, tabu_list_size=4, tabu_tenur=4):
+        for tabu_list_size in range(1, 9, 1):
+            for tabu_tenur in range(1, 9, 1):
+                """Perform a focused tabu search on  synonyms of words in the sentence, with tabu list and aspiration criteria."""
+                beam = [initial_result.attacked_text]
+                best_result = initial_result
+                tabu_list = []
+                tabu_list_size = (
+                    tabu_list_size  # Example size, adjust based on your needs
                 )
-                # Exclude transformations in the tabu list
-                transformations = [t for t in transformations if t not in tabu_list]
-                potential_next_beam += transformations
-            print("potential", potential_next_beam)
+                tabu_tenur = tabu_tenur
 
-            if len(potential_next_beam) == 0:
+                while not best_result.goal_status == GoalFunctionResultStatus.SUCCEEDED:
+                    potential_next_beam = []
+                    for text in beam:
+                        transformations = self.get_transformations(
+                            text, original_text=initial_result.attacked_text
+                        )
+                        # Exclude transformations in the tabu list
+                        transformations = [
+                            t for t in transformations if t not in tabu_list
+                        ]
+                        potential_next_beam += transformations
+                    # print("potential", potential_next_beam)
+
+                    if len(potential_next_beam) == 0:
+                        return best_result
+
+                    results, search_over = self.get_goal_results(potential_next_beam)
+                    scores = np.array([r.score for r in results])
+                    best_result = results[scores.argmax()]
+                    if search_over:
+                        return best_result
+
+                    best_indices = (-scores).argsort()[:tabu_tenur]
+                    beam = [potential_next_beam[i] for i in best_indices]
+
+                    # Update tabu list with the selected transformations
+                    for i in best_indices:
+                        if potential_next_beam[i] not in tabu_list:
+                            if len(tabu_list) >= tabu_list_size:
+                                tabu_list.pop(0)
+                            tabu_list.append(potential_next_beam[i])
+
                 return best_result
-
-            results, search_over = self.get_goal_results(potential_next_beam)
-            scores = np.array([r.score for r in results])
-            best_result = results[scores.argmax()]
-            if search_over:
-                return best_result
-
-            best_indices = (-scores).argsort()[:tabu_tenur]
-            beam = [potential_next_beam[i] for i in best_indices]
-
-            # Update tabu list with the selected transformations
-            for i in best_indices:
-                if potential_next_beam[i] not in tabu_list:
-                    if len(tabu_list) >= tabu_list_size:
-                        tabu_list.pop(0)
-                    tabu_list.append(potential_next_beam[i])
-
-        return best_result
+            print("tabu_tenur", tabu_tenur)
+            print("tabu_list_size", tabu_list_size)
 
     @property
     def is_black_box(self):
