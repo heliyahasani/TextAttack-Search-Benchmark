167cef881eeb8bdd693cb349b2c30eed5f7df521
Heliya Hasani <heliya@heraculesai.org>
2024-05-18 22:26:48 +0200

    tabu classic

--- grid_run.py
+++ grid_run.py
@@ -12,17 +12,17 @@ TRANSFORMATIONS = ["word-swap-wordnet", "word-swap-embedding", "word-swap-hownet
 CONSTRAINT_LEVEL = ["strict"]
 SEARCH_METHODS = {
     "tabu_classic": ["tabu4", "tabu8", "tabu16"],
-    "tabu_agglomerative": [
-        "tabu_agglomerative_average",
-        "tabu_agglomerative_complete",
-        "tabu_agglomerative_single",
-        "tabu_agglomerative_ward",
-    ],
-    "tabu_dbscan": ["tabu_dbscan"],
-    "tabu_dynamic_tenure": ["tabu_dynamic_tenure", "tabu_dynamic"],
-    "tabu_hdbscan": ["tabu_hdbscan"],
-    "tabu_kmeans": ["tabu_kmeans"],
-    "tabu_semantic_similarity": ["tabu_semantic_similarity"],
+    # "tabu_agglomerative": [
+    #     "tabu_agglomerative_average",
+    #     "tabu_agglomerative_complete",
+    #     "tabu_agglomerative_single",
+    #     "tabu_agglomerative_ward",
+    # ],
+    # "tabu_dbscan": ["tabu_dbscan"],
+    # "tabu_dynamic_tenure": ["tabu_dynamic_tenure", "tabu_dynamic"],
+    # "tabu_hdbscan": ["tabu_hdbscan"],
+    # "tabu_kmeans": ["tabu_kmeans"],
+    # "tabu_semantic_similarity": ["tabu_semantic_similarity"],
 }
 
 
